d118013450fe49f3f61633b4d56ac87cf770e130
Luolc <luolc.witty@gmail.com>
2019-07-06 03:05:59 +0800

    add layer norm before relu

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -540,6 +540,7 @@ class TransformerEncoderLayer(nn.Module):
         self.ffn = nn.ModuleList([
             nn.Sequential(
                 Linear(self.embed_dim, args.encoder_ffn_embed_dim // self.num_ffn),
+                LayerNorm(self.embed_dim),
                 nn.ReLU(),
                 nn.Dropout(p=self.activation_dropout),
                 Linear(args.encoder_ffn_embed_dim // self.num_ffn, self.embed_dim),
@@ -670,6 +671,7 @@ class TransformerDecoderLayer(nn.Module):
         self.ffn = nn.ModuleList([
             nn.Sequential(
                 Linear(self.embed_dim, args.decoder_ffn_embed_dim // self.num_ffn),
+                LayerNorm(self.embed_dim, export=export),
                 nn.ReLU(),
                 nn.Dropout(p=self.activation_dropout),
                 Linear(args.decoder_ffn_embed_dim // self.num_ffn, self.embed_dim),
