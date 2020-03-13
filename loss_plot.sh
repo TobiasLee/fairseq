d06d5be755e53271395157d8c1e7d63fee6daa1d
Tobias Lee <tobiaslee@foxmail.com>
2020-03-13 13:57:12 +0800

    rename

--- loss_plot.sh
+++ loss_plot.sh
@@ -2,29 +2,32 @@ ARCH=transformer_iwslt_de_en
 DATA_PATH=data-bin/iwslt14.tokenized.de-en.joined
 export PYTHONPATH=$PYTHONPATH:./plot/
 LR=0.0005
-GPU=1
+GPU="0"
 WU=4000 
-
-for SEED in 1234 
+NORM="pre"
+WITHWU='nowu'
+for SEED in 1234 2345 3456 3456 5678  
 do
 
 echo "seed=$SEED"
 echo "warm up updates=$WU"
-OUTPUT_PATH=checkpoints/IWSLT/post-norm-with-wu-${LR}-seed$SEED
+
+OUTPUT_PATH=checkpoints/IWSLT/$NORM-norm-$WITHWU-${LR}-seed$SEED
 #OUTPUT_PATH=checkpoints/deen_transformer_fp_822
-RESULT_PATH=results/IWSLT/post-norm-with-wu-${LR}-seed$SEED
+RESULT_PATH=results/IWSLT/$NORM-norm-$WITHWU-${LR}-seed$SEED
 #RESULT_PATH=results/IWSLT/zgx-deen-${LR}-seed$SEED
 
 mkdir -p $OUTPUT_PATH
 mkdir -p $RESULT_PATH
-echo "cleaning h5 file"
-# rm $OUTPUT_PATH/*.h5
-
-#--encoder-normalize-before --decoder-normalize-before \
-CUDA_VISIBLE_DEVICES=$GPU   python3 trans_plot.py   $DATA_PATH  \
+#echo "cleaning h5 file"
+# rm $OUTPUT_PATH/*.h5 # ckpt_best_init_valid_normalize_bert_weights_xignore=biasbn_xnorm=filter_inital_direction_normlize_bert_yignore=biasbn_ynorm=filter.h5
+#    --dir-file "$OUTPUT_PATH/ckpt_best_init_valid_normalize_bert_weights_xignore=biasbn_xnorm=filter_inital_direction_normlize_bert_yignore=biasbn_ynorm=filter.h5" \
+#    # --dir-file 'post-nowu-dir.h5'\
+CUDA_VISIBLE_DEVICES=$GPU  python3 plot_loss_surface.py   $DATA_PATH --init-model  --normalize-bert --surf-file "$OUTPUT_PATH/ckpt_init_loss_41x41.h5" \
+    --encoder-normalize-before --decoder-normalize-before \
     --dir-type weights --xnorm filter --xignore biasbn --ynorm filter --yignore biasbn  --plot\
-    --x=-1:1:51 --y=-1:1:51 --ngpu 1 --valid-subset valid\
-    --seed $SEED --model-file $OUTPUT_PATH/checkpoint_best.pt  \
+    --x=-2:2:41 --y=-2:2:41 --ngpu 1 --valid-subset valid\
+    --seed $SEED --model-file $OUTPUT_PATH/initpoint_valid_normalize_bert \
     -a $ARCH  --share-all-embeddings \
     --optimizer adam --lr $LR \
     -s de -t en \
@@ -34,7 +37,7 @@ CUDA_VISIBLE_DEVICES=$GPU   python3 trans_plot.py   $DATA_PATH  \
     --criterion label_smoothed_cross_entropy \
     --max-update 50000 \
     --warmup-updates $WU --warmup-init-lr '5e-4' \
-    --adam-betas '(0.9, 0.98)' --save-dir $OUTPUT_PATH --restore-file $OUTPUT_PATH/checkpoint_last.pt --reset-optimizer \
+    --adam-betas '(0.9, 0.98)' --save-dir $OUTPUT_PATH --restore-file $OUTPUT_PATH/checkpoint_best_fake.pt --reset-optimizer \
     --no-progress-bar --log-interval 100 \
     --ddp-backend=no_c10d 2>&1 | tee -a $OUTPUT_PATH/train_log.txt 
     
---
+++ plot_loss_surface.py
@@ -0,0 +1,446 @@
+#!/usr/bin/env python3 -u
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the LICENSE file in
+# the root directory of this source tree. An additional grant of patent rights
+# can be found in the PATENTS file in the same directory.
+"""
+Train a new model on one or across multiple GPUs.
+"""
+
+import collections
+import copy
+import math
+import os
+import random
+import time
+
+import h5py
+import numpy as np
+import torch
+
+from fairseq import checkpoint_utils, distributed_utils, options, progress_bar, tasks, utils
+from fairseq.trainer import Trainer
+from fairseq.meters import AverageMeter, StopwatchMeter
+from fairseq.models.transformer import TransformerModel
+
+from plot import net_plotter
+from plot import scheduler
+from plot import mpi4pytorch as mpi
+from plot.plot_surface import name_surface_file, setup_surface_file
+from plot import projection as proj
+from plot import plot_2D
+
+
+def main(args, init_distributed=False):
+    utils.import_user_module(args)
+
+    assert args.max_tokens is not None or args.max_sentences is not None, \
+        'Must specify  batch size either with --max-tokens or --max-sentences'
+
+    # Initialize CUDA and distributed training
+    if torch.cuda.is_available() and not args.cpu:
+        torch.cuda.set_device(args.device_id)
+    torch.manual_seed(args.seed)
+    if init_distributed:
+        args.distributed_rank = distributed_utils.distributed_init(args)
+
+    # Print args
+    print(args)
+    try:
+        args.xmin, args.xmax, args.xnum = [int(a) for a in args.x.split(':')]
+        print(args.xmin, args.xmax, args.xnum)
+        args.ymin, args.ymax, args.ynum = (None, None, None)
+        if args.y:
+            args.ymin, args.ymax, args.ynum = [int(a) for a in args.y.split(':')]
+            assert args.ymin and args.ymax and args.ynum, \
+                'You specified some arguments for the y axis, but not all'
+    except:
+        raise Exception('Improper format for x- or y-coordinates. Try something like -1:1:51')
+    # Setup task, e.g., translation, language modeling, etc.
+    task = tasks.setup_task(args)
+
+    # Load valid dataset (we load training data below, based on the latest checkpoint)
+    for valid_sub_split in args.valid_subset.split(','):
+        task.load_dataset(valid_sub_split, combine=False, epoch=0)
+
+    # Build model and criterion
+    model = task.build_model(args)
+    criterion = task.build_criterion(args)
+    print(model)
+    print('| model {}, criterion {}'.format(args.arch, criterion.__class__.__name__))
+    print('| num. model params: {} (num. trained: {})'.format(
+        sum(p.numel() for p in model.parameters()),
+        sum(p.numel() for p in model.parameters() if p.requires_grad),
+    ))
+
+    # Build trainer
+    trainer = Trainer(args, task, model, criterion)
+    print('| training on {} GPUs'.format(args.distributed_world_size))
+    print('| max tokens per GPU = {} and max sentences per GPU = {}'.format(
+        args.max_tokens,
+        args.max_sentences,
+    ))
+
+    # Load the latest checkpoint if one is available and restore the
+    # corresponding train iterator
+    # extra_state, epoch_itr = checkpoint_utils.load_checkpoint(args, trainer)
+    extra_state = trainer.load_checkpoint(
+        args.restore_file,
+        args.reset_optimizer,
+        args.reset_lr_scheduler,
+        eval(args.optimizer_overrides),
+        reset_meters=args.reset_meters,
+    )
+    epoch_itr = trainer.get_train_iterator(epoch=0)
+
+    train_meter = StopwatchMeter()
+    train_meter.start()
+
+    # mpi setting
+    if args.mpi:
+        comm = mpi.setup_MPI()
+        rank, nproc = comm.Get_rank(), comm.Get_size()
+    else:
+        comm, rank, nproc = None, 0, 1
+
+    # --------------------------------------------------------------------------
+    # Setup the direction file and the surface file
+    # --------------------------------------------------------------------------
+    if args.dir_file :
+        dir_file = args.dir_file
+    else:
+        dir_file = net_plotter.name_direction_file(args)  # name the direction file
+    if rank == 0:
+        init_net = TransformerModel.build_model(args, task) if args.init_model else None
+        if torch.cuda.is_available():
+            init_net.cuda()
+        print('setting dir file... init_model: ', args.init_model)
+        net_plotter.setup_direction(args, dir_file, model, init_net)
+    #print('dir file set')
+   #  exit(0)
+    surf_file = name_surface_file(args, dir_file)
+    if rank == 0:
+        setup_surface_file(args, surf_file, dir_file)
+
+    # wait until master has setup the direction file and surface file
+    mpi.barrier(comm)
+
+    # load directions
+    direction = net_plotter.load_directions(dir_file)
+    # calculate the consine similarity of the two directions
+    if len(direction) == 2 and rank == 0:
+        similarity = proj.cal_angle(proj.nplist_to_tensor(direction[0]),
+                                    proj.nplist_to_tensor(direction[1]))
+        print('cosine similarity between x-axis and y-axis: %f' % similarity)
+
+    dataset2plot = args.valid_subset.split(',')[0]  # we use valid_subset as the dataset to plot on
+    weight = net_plotter.get_weights(model)  # initial parameters
+    state = copy.deepcopy(model.state_dict())  # deepcopy since state_dict are references
+    plot(surf_file, model, weight, state, direction,
+         comm, rank, args,
+         trainer, task, epoch_itr, dataset2plot,
+         loss_key='loss')
+    if args.plot:
+        print('plotting....')
+        plot_2D.plot_2d_contour(surf_file, 'loss', args.vmin, args.vmax, args.vlevel, args.show)
+
+    # print('| done plotting in {:.1f} seconds'.format(train_meter.sum))
+
+
+def validate(args, trainer, task, epoch_itr, subsets):
+    """Evaluate the model on the validation set(s) and return the losses."""
+    valid_losses = []
+    for subset in subsets:
+        # Initialize data iterator
+        itr = task.get_batch_iterator(
+            dataset=task.dataset(subset),
+            max_tokens=args.max_tokens_valid,
+            max_sentences=args.max_sentences_valid,
+            max_positions=utils.resolve_max_positions(
+                task.max_positions(),
+                trainer.get_model().max_positions(),
+            ),
+            ignore_invalid_inputs=args.skip_invalid_size_inputs_valid_test,
+            required_batch_size_multiple=args.required_batch_size_multiple,
+            seed=args.seed,
+            num_shards=args.distributed_world_size,
+            shard_id=args.distributed_rank,
+            num_workers=args.num_workers,
+        ).next_epoch_itr(shuffle=False)
+        progress = progress_bar.build_progress_bar(
+            args, itr, epoch_itr.epoch,
+            prefix='valid on \'{}\' subset'.format(subset),
+            no_progress_bar='simple'
+        )
+
+        # reset validation loss meters
+        for k in ['valid_loss', 'valid_nll_loss']:
+            meter = trainer.get_meter(k)
+            if meter is not None:
+                meter.reset()
+        extra_meters = collections.defaultdict(lambda: AverageMeter())
+
+        for sample in progress:
+            log_output = trainer.valid_step(sample)
+
+            for k, v in log_output.items():
+                if k in ['loss', 'nll_loss', 'ntokens', 'nsentences', 'sample_size']:
+                    continue
+                extra_meters[k].update(v)
+
+        # log validation stats
+        stats = get_valid_stats(trainer)
+        for k, meter in extra_meters.items():
+            stats[k] = meter.avg
+        progress.print(stats, tag=subset, step=trainer.get_num_updates())
+
+        valid_losses.append(stats[args.best_checkpoint_metric].avg)
+    return valid_losses
+
+
+def evaluate_loss(args, trainer, task, epoch_itr, subset):
+    """Evaluate the model on the validation set(s) and return the losses."""
+    # Initialize data iterator
+    itr = task.get_batch_iterator(
+        dataset=task.dataset(subset),
+        max_tokens=args.max_tokens_valid,
+        max_sentences=args.max_sentences_valid,
+        max_positions=utils.resolve_max_positions(
+            task.max_positions(),
+            trainer.get_model().max_positions(),
+        ),
+        ignore_invalid_inputs=args.skip_invalid_size_inputs_valid_test,
+        required_batch_size_multiple=args.required_batch_size_multiple,
+        seed=args.seed,
+        num_shards=args.distributed_world_size,
+        shard_id=args.distributed_rank,
+        num_workers=args.num_workers,
+    ).next_epoch_itr(shuffle=False)
+    progress = progress_bar.build_progress_bar(
+        args, itr, epoch_itr.epoch,
+        prefix='valid on \'{}\' subset'.format(subset),
+        no_progress_bar='simple'
+    )
+
+    # reset validation loss meters
+    for k in ['valid_loss', 'valid_nll_loss']:
+        meter = trainer.get_meter(k)
+        if meter is not None:
+            meter.reset()
+    extra_meters = collections.defaultdict(lambda: AverageMeter())
+
+    for sample in progress:
+        log_output = trainer.valid_step(sample)
+
+        for k, v in log_output.items():
+            if k in ['loss', 'nll_loss', 'ntokens', 'nsentences', 'sample_size']:
+                continue
+            extra_meters[k].update(v)
+
+        # log validation stats
+    stats = get_valid_stats(trainer)
+    for k, meter in extra_meters.items():
+        stats[k] = meter.avg
+    progress.print(stats, tag=subset, step=trainer.get_num_updates())
+    # directly return the loss avg
+    return stats['loss'].avg
+
+
+def plot(surf_file, model, weight, state, direction,
+         comm, rank, args,
+         trainer, task, epoch_itr, subset, loss_key='loss'):
+    # integrate crunch function here
+    """
+            Calculate the loss values and accuracies of modified models in parallel
+            using MPI reduce.
+        """
+
+    f = h5py.File(surf_file, 'r+' if rank == 0 else 'r')
+    losses, accuracies = [], []
+    xcoordinates = f['xcoordinates'][:]
+    ycoordinates = f['ycoordinates'][:] if 'ycoordinates' in f.keys() else None
+
+    if loss_key not in f.keys():
+        shape = xcoordinates.shape if ycoordinates is None else (len(xcoordinates), len(ycoordinates))
+        losses = -np.ones(shape=shape)
+        # accuracies = -np.ones(shape=shape)
+        if rank == 0:
+            f[loss_key] = losses
+            # f[acc_key] = accuracies
+    else:
+        losses = f[loss_key][:]
+        # accuracies = f[acc_key][:]
+
+    # Generate a list of indices of 'losses' that need to be filled in.
+    # The coordinates of each unfilled index (with respect to the direction vectors
+    # stored in 'd') are stored in 'coords'.
+    inds, coords, inds_nums = scheduler.get_job_indices(losses, xcoordinates, ycoordinates, comm)
+
+    print('Computing %d values for rank %d' % (len(inds), rank))
+    start_time = time.time()
+    total_sync = 0.0
+
+    # criterion = nn.CrossEntropyLoss()
+    # if args.loss_name == 'mse':
+    #     criterion = nn.MSELoss()
+
+    # Loop over all uncalculated loss values
+    for count, ind in enumerate(inds):
+        # Get the coordinates of the loss value being calculated
+        coord = coords[count]
+
+        # Load the weights corresponding to those coordinates into the net
+        if args.dir_type == 'weights':
+            net_plotter.set_weights(model if args.ngpu > 1 else model, weight, direction, coord)
+        elif args.dir_type == 'states':
+            net_plotter.set_states(model if args.ngpu > 1 else model, state, direction, coord)
+
+        # Record the time to compute the loss value
+        loss_start = time.time()
+        loss = evaluate_loss(args, trainer, task, epoch_itr, subset)
+        # loss, acc = evaluation.eval_loss(net, criterion, dataloader, args.cuda)
+        loss_compute_time = time.time() - loss_start
+
+        # Record the result in the local array
+        losses.ravel()[ind] = loss
+        # accuracies.ravel()[ind] = acc
+
+        # Send updated plot data to the master node
+        syc_start = time.time()
+        losses = mpi.reduce_max(comm, losses)
+        # accuracies = mpi.reduce_max(comm, accuracies)
+        syc_time = time.time() - syc_start
+        total_sync += syc_time
+
+        # Only the master node writes to the file - this avoids write conflicts
+        if rank == 0:
+            f[loss_key][:] = losses
+            # f[acc_key][:] = accuracies
+            f.flush()
+
+        print('Evaluating rank %d  %d/%d  (%.1f%%)  coord=%s \t%s= %.3f  \ttime=%.2f \tsync=%.2f' % (
+            rank, count, len(inds), 100.0 * count / len(inds), str(coord), loss_key, loss,
+            loss_compute_time, syc_time))
+
+    # This is only needed to make MPI run smoothly. If this process has less work than
+    # the rank0 process, then we need to keep calling reduce so the rank0 process doesn't block
+    for i in range(max(inds_nums) - len(inds)):
+        losses = mpi.reduce_max(comm, losses)
+
+    total_time = time.time() - start_time
+    print('Rank %d done!  Total time: %.2f Sync: %.2f' % (rank, total_time, total_sync))
+
+    f.close()
+
+
+def get_valid_stats(trainer):
+    stats = collections.OrderedDict()
+    stats['loss'] = trainer.get_meter('valid_loss')
+    if trainer.get_meter('valid_nll_loss').count > 0:
+        nll_loss = trainer.get_meter('valid_nll_loss')
+        stats['nll_loss'] = nll_loss
+    else:
+        nll_loss = stats['loss']
+    stats['ppl'] = utils.get_perplexity(nll_loss.avg)
+    stats['num_updates'] = trainer.get_num_updates()
+    if hasattr(checkpoint_utils.save_checkpoint, 'best'):
+        stats['best_loss'] = min(
+            checkpoint_utils.save_checkpoint.best, stats['loss'].avg)
+    return stats
+
+
+def distributed_main(i, args, start_rank=0):
+    args.device_id = i
+    if args.distributed_rank is None:  # torch.multiprocessing.spawn
+        args.distributed_rank = start_rank + i
+    main(args, init_distributed=True)
+
+
+def cli_main():
+    parser = options.get_training_parser()
+    parser.add_argument('--mpi', '-m', action='store_true', help='use mpi')
+    # parser.add_argument('--cuda', '-c', action='store_true', help='use cuda')
+    parser.add_argument('--threads', default=2, type=int, help='number of threads')
+    parser.add_argument('--ngpu', type=int, default=1,
+                         help='number of GPUs to use for each rank, useful for data parallel evaluation')
+    parser.add_argument('--batch_size', default=128, type=int, help='minibatch size')
+
+    # data parameters
+    parser.add_argument('--split_idx', default=0, type=int, help='the index of data splits for the dataloader')
+    # parser.add_argument('--trainloader', default='', help='path to the dataloader with random labels')
+    # parser.add_argument('--testloader', default='', help='path to the testloader with random labels')
+
+    # model parameters
+    parser.add_argument('--model-folder', default='', help='the common folder that contains model_file and model_file2')
+    parser.add_argument('--model-file', default='', help='path to the trained model file')
+    parser.add_argument('--model-file2', default='', help='use (model_file2 - model_file) as the xdirection')
+    parser.add_argument('--model-file3', default='', help='use (model_file3 - model_file) as the ydirection')
+    parser.add_argument('--init-model', action='store_true', default=False, help='x direction is trained model - initial model')
+    # direction parameters
+    parser.add_argument('--dir-file', default='',
+                        help='specify the name of direction file, or the path to an eisting direction file')
+    parser.add_argument('--dir-type', default='weights',
+                        help='direction type: weights | states (including BN\'s running_mean/var)')
+    parser.add_argument('--x', default='-1:1:51', help='A string with format xmin:x_max:xnum')
+    parser.add_argument('--y', default=None, help='A string with format ymin:ymax:ynum')
+    parser.add_argument('--xnorm', default='', help='direction normalization: filter | layer | weight')
+    parser.add_argument('--ynorm', default='', help='direction normalization: filter | layer | weight')
+    parser.add_argument('--xignore', default='', help='ignore bias and BN parameters: biasbn')
+    parser.add_argument('--yignore', default='', help='ignore bias and BN parameters: biasbn')
+    parser.add_argument('--same_dir', action='store_true', default=False,
+                        help='use the same random direction for both x-axis and y-axis')
+    parser.add_argument('--idx', default=0, type=int, help='the index for the repeatness experiment')
+    parser.add_argument('--surf-file', default='',
+                        help='customize the name of surface file, could be an existing file.')
+    parser.add_argument('--normalize-bert', action='store_true', default=False,
+                        help='whether normalize ydirection by xnorm / ynorm')
+
+    # plot parameters
+    parser.add_argument('--proj-file', default='', help='the .h5 file contains projected optimization trajectory.')
+    parser.add_argument('--loss-max', default=5, type=float, help='Maximum value to show in 1D plot')
+    parser.add_argument('--vmax', default=10, type=float, help='Maximum value to map')
+    parser.add_argument('--vmin', default=0.1, type=float, help='Miminum value to map')
+    parser.add_argument('--vlevel', default=0.5, type=float, help='plot contours every vlevel')
+    parser.add_argument('--show', action='store_true', default=False, help='show plotted figures')
+    parser.add_argument('--log', action='store_true', default=False, help='use log scale for loss values')
+    parser.add_argument('--plot', action='store_true', default=False, help='plot figures after computation')
+
+    args = options.parse_args_and_arch(parser)
+
+    if args.distributed_init_method is None:
+        distributed_utils.infer_init_method(args)
+
+    if args.distributed_init_method is not None:
+        # distributed training
+        if torch.cuda.device_count() > 1 and not args.distributed_no_spawn:
+            start_rank = args.distributed_rank
+            args.distributed_rank = None  # assign automatically
+            torch.multiprocessing.spawn(
+                fn=distributed_main,
+                args=(args, start_rank),
+                nprocs=torch.cuda.device_count(),
+            )
+        else:
+            distributed_main(args.device_id, args)
+    elif args.distributed_world_size > 1:
+        # fallback for single node with multiple GPUs
+        assert args.distributed_world_size <= torch.cuda.device_count()
+        port = random.randint(10000, 20000)
+        args.distributed_init_method = 'tcp://localhost:{port}'.format(port=port)
+        args.distributed_rank = None  # set based on device id
+        if max(args.update_freq) > 1 and args.ddp_backend != 'no_c10d':
+            print('| NOTE: you may get better performance with: --ddp-backend=no_c10d')
+        torch.multiprocessing.spawn(
+            fn=distributed_main,
+            args=(args,),
+            nprocs=args.distributed_world_size,
+        )
+    else:
+        # single GPU training
+        main(args)
+
+
+if __name__ == '__main__':
+    cli_main()
